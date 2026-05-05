0ce9dab6762be381a08e7817cb317277bfc4526b
Riski153 <m.riski.f21@mail.umy.ac.id>
2026-05-05 11:02:07 +0700

    menambahkan isi class layang2

--- BAngundatar/bangundatar.cpp
+++ BAngundatar/bangundatar.cpp
@@ -3,4 +3,9 @@ using namespace std;
 
 class BelahKetupat; // forward declaration
 
-class LayangLayang
+class LayangLayang {
+private:
+    float d1, d2; // diagonal
+    float a, b;   // sisi
+
+};
